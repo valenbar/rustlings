f487a632b435168969a18f24fbe1f34c23f8e5c6
Valentin Bartschies <valentin.bartschies@tuhh.de>
2024-06-02 15:43:00 +0200

    add solution for quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,16 +16,31 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
+pub trait ReportCard {
+    fn print(&self) -> String; 
+}
+
+pub struct ReportCardNumber {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+pub struct ReportCardLetter {
+    pub grade: String,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl ReportCard for ReportCardNumber {
+    fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade)
+    }
+}
+impl ReportCard for ReportCardLetter {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
@@ -37,8 +52,8 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = ReportCardNumber {
+            grade: 2.1, 
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -51,8 +66,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = ReportCardLetter {
+            grade: String::from("A+"),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
